061414864643a1510afb7107dd483d82e46802ea
ramamimu <ahhmaad.blogger@gmail.com>
2023-06-11 19:58:16 +0700

    feat: open server and socket

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+// Set static folder
+app.use(express.static("public"));
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
